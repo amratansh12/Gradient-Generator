fc23a76734427973d0bf693efd9356385439108a
Amratansh Shrivastava <ashri1205@gmail.com>
2023-01-09 23:41:24 +0530

    Update script.js
    Added an option of selecting the type of gradient.

--- script.js
+++ script.js
@@ -2,13 +2,28 @@ let h4 = document.getElementById("cssInfo");
 let body = document.getElementById("body");
 let color1 = document.getElementById("color1");
 let color2 = document.getElementById("color2");
+let select = document.getElementById("direction");
+let css;
+console.log(select.value);
 
 function colorChange(){
-	document.body.style.backgroundImage = "linear-gradient(to right,"+ color1.value + "," + color2.value + ")";
-	h4.innerText = "CSS : linear-gradient(to right, " + color1.value + ", " + color2.value + " )"
+	if(select.value === "Linear Gradients"){
+		css = "linear-gradient(to right, " + color1.value + ", " + color2.value + " )";
+	}
+	else if(select.value === "Radial Gradients"){
+		css = "radial-gradient(" + color1.value + ", " + color2.value + " )";
+	}
+	else if(select.value === "Conic Gradients"){
+		css = "conic-gradient(" + color1.value + ", " + color2.value + " )";
+	}
+	document.body.style.backgroundImage = css;
+	h4.innerText = "CSS : linear-gradient(to right, " + color1.value + ", " + color2.value + " )";
 }
 
 color1.addEventListener("input", colorChange);
 
 color2.addEventListener("input", colorChange);
+
+
+
 
